91a2336c52fdede789b18f625e6d5ede8614999d
kaave <abe@framelunch.jp>
2018-03-04 15:36:58 +0900

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  moduleFileExtensions: ['tsx', 'ts', 'js', 'json'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '\\.spec\\.(ts|tsx)$',
+};
---
+++ src/index.spec.ts
@@ -0,0 +1,7 @@
+import add from './index';
+
+describe('add', () => {
+  it('will be success im happy!', () => {
+    expect(add(10, 20)).toBe(30);
+  });
+});
