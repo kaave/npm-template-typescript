8f429e367055d2a842484fd7587b74b083f30c85
kaave <junkjunctions@gmail.com>
2020-10-11 20:23:21 +0900

    upgrade

--- dist/index.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const index_1 = tslib_1.__importDefault(require("~/index"));
-describe('add', () => {
-    it('will be success im happy!', () => {
-        expect(index_1.default(10, 20)).toBe(30);
-    });
-});
