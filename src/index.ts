9d08e2e7a6ec6d75f3599bab4b54d26e66555c26
kaave <junkjunctions@gmail.com>
2021-04-29 18:50:16 +0900

    :add: totalphotos resolvers

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('Hello, world!');
+import { ApolloServer } from 'apollo-server';
+import { readFileSync } from 'fs';
+import path from 'path';
+import type { Resolvers } from './generated/resolvers';
+
+const typeDefs = readFileSync(path.join(__dirname, 'typeDefs.graphql')).toString();
+
+const resolvers: Resolvers = {
+  Query: {
+    totalPhotos() {
+      return 42;
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+async function main() {
+  const { url } = await server.listen();
+  console.log('GraphQL service running on', url);
+}
+
+main();
