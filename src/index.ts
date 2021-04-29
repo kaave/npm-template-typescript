e68228889c3257a9d556e1923b64981d0f45dc2c
kaave <junkjunctions@gmail.com>
2021-04-29 23:32:20 +0900

    add: simple mutation

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,37 @@
 import { ApolloServer } from 'apollo-server';
 import { readFileSync } from 'fs';
+import { nanoid } from 'nanoid';
 import path from 'path';
-import type { Resolvers } from './generated/resolvers';
+import type { MutationResolvers, QueryResolvers, Resolvers } from './generated/resolvers';
 
 const typeDefs = readFileSync(path.join(__dirname, 'typeDefs.graphql')).toString();
 
-const resolvers: Resolvers = {
-  Query: {
-    totalPhotos() {
-      return 42;
+const queryResolvers: QueryResolvers = {
+  totalPhotos: () => 42,
+};
+
+const mutationResolvers: MutationResolvers = {
+  postPhoto: (_, { input: { name, category, description } }) => ({
+    id: nanoid(),
+    name,
+    url: 'http://localhost',
+    description: description ?? '',
+    category: category ?? 'SELFIE',
+    postedBy: {
+      githubLogin: '',
+      name: 'PostPhotoUser',
+      avatar: 'http://localhost/image',
+      postedPhotos: [],
+      inPhotos: [],
     },
-  },
+    taggedUsers: [],
+    created: new Date(),
+  }),
+};
+
+const resolvers: Resolvers = {
+  Query: queryResolvers,
+  Mutation: mutationResolvers,
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
